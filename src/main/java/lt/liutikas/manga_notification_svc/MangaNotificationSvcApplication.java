655b0521ed2e83865009ddd50b3037ba949e37f7
Guguskis <matasliutikas@gmail.com>
2024-11-08 19:00:11 +0200

    init commit

---
+++ src/main/java/lt/liutikas/manga_notification_svc/MangaNotificationSvcApplication.java
@@ -0,0 +1,13 @@
+package lt.liutikas.manga_notification_svc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MangaNotificationSvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MangaNotificationSvcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lt/liutikas/manga_notification_svc/MangaNotificationSvcApplicationTests.java
@@ -0,0 +1,13 @@
+package lt.liutikas.manga_notification_svc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MangaNotificationSvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
